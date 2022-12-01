09ffebbd72a997202dc4926cb6b411471c9f00b4
Alex <alex_sizow@outlook.com>
2022-12-01 21:42:52 +0300

    commits

--- components/Elements/inputSearch/inputsearch.jsx
+++ components/Elements/inputSearch/inputsearch.jsx
@@ -9,42 +9,23 @@ const InputSearch = ({ active, setActive }) => {
 			onClick={() => setActive(false)}>
 			<form className={styles.inputsearch}>
 				<div className={styles.inputsearch__town}>
-					<input
-						type='text'
-						placeholder='Daugavpils'></input>
+					<input type='text' placeholder='Daugavpils'></input>
 					<Image
 						src='/TwoArrow.svg'
 						alt='Picture of the author'
 						width={22}
 						height={22}
 					/>
-					<input
-						type='text'
-						placeholder='Daugavpils'></input>
+					<input type='text' placeholder='Daugavpils'></input>
 				</div>
 				<div className={styles.inputsearch__city}>
-					<input
-						type='text'
-						placeholder='Baldone'
-					/>
-					<input
-						type='text'
-						placeholder='Baldone'
-					/>
-					<input
-						type='text'
-						placeholder='Baldone'
-					/>
+					<input type='text' placeholder='Baldone' />
+					<input type='text' placeholder='Baldone' />
+					<input type='text' placeholder='Baldone' />
 				</div>
 				<div className={styles.inputsearch__time}>
-					<input
-						type='text'
-						placeholder='28.10.2022'
-					/>
-					<input
-						type='text'
-						placeholder='28.10.2022'
-					/>
+					<input type='text' placeholder='28.10.2022' />
+					<input type='text' placeholder='28.10.2022' />
 				</div>
 				<div className={styles.inputsearch__timeslider}>
 					<TimeSlider />
